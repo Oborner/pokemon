b8af60eae7b0530052aafc5ff4972a65d73ec46e
Oborner <94976381+Oborner@users.noreply.github.com>
2022-08-12 21:04:02 -0300

    alfa 0.1

--- src/pokemon.rs
+++ src/pokemon.rs
@@ -1,79 +1,160 @@
-#[derive(Debug)]
+use std::clone::Clone;
+mod eficiencia;
+use crate::pokemon::eficiencia::eficiencia;
+use rand::prelude::*;
+
+pub struct Jogador{
+    pub nome: String,
+    pub pokemons: Vec<Pokemon>,
+    pub bag: Vec<Itens>,
+}
+#[derive(Debug, Clone)]
 pub struct Pokemon{
     pub nome: String,
+    pub hp_max: u16,
     pub hp: u16,
-    pub atack: f32,
+    pub attack: f32,
     pub defense: f32,
-    pub sp_atack: f32,
+    pub sp_attack: f32,
     pub sp_defense: f32,
     pub speed: f32,
+    pub p_tipo: Tipo,
     pub attacks: Vec<Attack>
+}
+
+pub struct Itens{
+    pub nome: String,
+    pub quant: u8,
+    pub cura: u8,
 }
 
 #[derive(Debug, Clone)]
 pub struct Attack{
-    pub name: String,
-    pub tipo: Tipo,
+    pub nome: String,
+    pub categoria: Categoria,
     pub power: f32,
+    pub pp: u8,
+    pub tipo: Tipo,
 }
 
-#[derive(Debug)]
-#[derive(PartialEq, Clone)]
+
+#[derive(Debug, PartialEq, Clone, Copy)]
 pub enum Tipo{
+    Normal,
+    Fire,
+    Water,
+    Grass,
+    Electric,
+    Ice,
+    Fighting,
+    Poison,
+    Ground,
+    Flying,
+    Psychic,
+    Bug,
+    Rock,
+    Ghost,
+    Dragon,
+    Dark,
+    Steel,
+    Fairy,
+}
+
+#[derive(Debug, PartialEq, Clone, Copy)]
+pub enum Categoria{
     Físico,
     Especial,
 }
 
 pub trait Ataque {
-    fn ataca(&self, inimigo: &mut Self, ind:u16);
+    fn ataca(atacante: &mut Self, inimigo: &mut Self, ind:usize);
+}
+
+impl Jogador {
+    pub fn novo (nome:String, pokemons:Vec<Pokemon>, bag:Vec<Itens>)-> Self{
+        Jogador{
+            nome,
+            pokemons,
+            bag,
+        }
+    }
 }
 
 impl Pokemon{
-    pub fn novo(nome:String, hp: u16, atack: f32, sp_defense: f32, sp_atack: f32, defense: f32, speed: f32, attacks: Vec<Attack>)-> Self{
+    pub fn novo(nome:String, hp: u16, hp_max:u16, attack: f32, defense: f32, sp_attack: f32, sp_defense: f32, speed: f32, p_tipo:Tipo, attacks: Vec<Attack>)-> Self{
         Pokemon{
-            nome: nome,
-            hp: hp,
-            atack: atack,
+            nome,
+            hp,
+            hp_max,
+            attack,
             sp_defense,
-            sp_atack,
-            defense: defense,
-            speed: speed,
+            sp_attack,
+            defense,
+            speed,
+            p_tipo,
             attacks,
         }
     }
 }
 
 impl  Attack{
-    pub fn novo (name:String, tipo:Tipo, power:f32) -> Self{
+    pub fn novo (nome:String, categoria:Categoria, power:f32, pp:u8, tipo:Tipo) -> Self{
         Attack{
-            name,
-            tipo,
+            nome,
+            categoria,
             power,
+            pp,
+            tipo
         }
     }
     
 }
 
-impl Ataque for Pokemon{
-    fn ataca(&self, inimigo: &mut Self, i: u16){
-        let a:usize = i.into();
-        let tipo = &self.attacks[a].tipo;
-        let dano = &self.attacks[a].power;
-        let cem:f32 = 100.000;
-        if tipo == &Tipo::Físico{
-            if inimigo.hp <= (dano*(cem/(cem + inimigo.defense))) as u16 {
-                inimigo.hp = 0;
-            } else {
-                inimigo.hp -= (dano*(cem/(cem + inimigo.defense))) as u16;
-            }
-        }else{
-            if inimigo.hp <= (dano*(cem/(cem + inimigo.sp_defense))) as u16 {
-                inimigo.hp = 0;
-            } else {
-                inimigo.hp -= (dano*(cem/(cem + inimigo.sp_defense))) as u16;
-            }
+impl Itens{
+    pub fn novo (nome:String, quant:u8, cura:u8)-> Self{
+        Itens{
+            nome,
+            quant,
+            cura
         }
-        println!("{:?}", inimigo.hp);
+    }
+}
 
+
+impl Ataque for Pokemon{
+    fn ataca(atacante: &mut Self, inimigo: &mut Self, i: usize){
+        let ataque_categoria = atacante.attacks[i].categoria.clone();
+        let poder = atacante.attacks[i].power.clone();
+        let tipo_ataque = atacante.attacks[i].tipo.clone();
+        let tipo_inimigo;
+        let p_ataque;
+        let p_defesa;
+        let random:f32 = rand::thread_rng().gen_range(85.0..101.0)/100.0;
+
+        tipo_inimigo = inimigo.p_tipo.clone();
+
+        if ataque_categoria == Categoria::Físico {
+            p_ataque = atacante.attack.clone();
+            p_defesa = inimigo.defense.clone();
+        } else {
+            p_ataque = atacante.sp_attack.clone();
+            p_defesa = inimigo.sp_defense.clone();
+        }
+
+        let modificador = eficiencia(tipo_ataque, tipo_inimigo);
+        let mut damage = (((((2.0*75.0/5.0)+2.0)*poder*(p_ataque/p_defesa))/50.0)+2.0)*random*modificador;
+
+        damage = damage.ceil();
+        let damage_int = damage as u16;
+
+        if damage_int>inimigo.hp{
+            inimigo.hp = 0;
+        } else {
+            inimigo.hp -= damage_int;
+        }
+
+        atacante.attacks[i].pp -= 1;
+
+        println!("{:?}", inimigo.hp);
     }
 }
---
+++ src/pokemon/eficiencia.rs
@@ -0,0 +1,29 @@
+use super::Tipo;
+
+
+pub fn eficiencia (t1:Tipo, t2:Tipo) -> f32{
+    let mut eficiencia:f32 = 1.0;
+
+    match t1 {
+        Tipo::Normal => if t2==Tipo::Rock || t2==Tipo::Steel{eficiencia=0.5} else if t2==Tipo::Ghost{eficiencia=0.0},
+        Tipo::Fire => if t2==Tipo::Fire || t2==Tipo::Water || t2==Tipo::Rock || t2==Tipo::Dragon{eficiencia=0.5} else if t2==Tipo::Grass || t2==Tipo::Ice || t2==Tipo::Bug || t2==Tipo::Steel{eficiencia=2.0},
+        Tipo::Water => if t2==Tipo::Water || t2==Tipo::Grass || t2==Tipo::Dragon{eficiencia=0.5} else if t2==Tipo::Fire || t2==Tipo::Ground || t2==Tipo::Rock{eficiencia=2.0},
+        Tipo::Grass => if t2==Tipo::Fire || t2==Tipo::Grass || t2==Tipo::Poison || t2==Tipo::Flying || t2==Tipo::Bug || t2==Tipo::Dragon || t2==Tipo::Steel{eficiencia=0.5} else if t2==Tipo::Water || t2==Tipo::Ground || t2==Tipo::Rock{eficiencia=2.0},
+        Tipo::Electric => if t2==Tipo::Grass || t2==Tipo::Electric || t2==Tipo::Dragon{eficiencia=0.5} else if t2==Tipo::Water || t2==Tipo::Flying{eficiencia=2.0} else if t2==Tipo::Ground{eficiencia=0.0},
+        Tipo::Ice => if t2==Tipo::Fire || t2==Tipo::Water || t2==Tipo::Ice || t2==Tipo::Steel{eficiencia=0.5} else if t2==Tipo::Grass || t2==Tipo::Ground || t2==Tipo::Flying || t2==Tipo::Dragon{eficiencia=2.0},
+        Tipo::Fighting => if t2==Tipo::Poison || t2==Tipo::Flying || t2==Tipo::Psychic || t2==Tipo::Bug || t2==Tipo::Fairy{eficiencia=0.5} else if t2==Tipo::Normal || t2==Tipo::Ice || t2==Tipo::Rock || t2==Tipo::Dark || t2==Tipo::Steel{eficiencia=2.0} else if t2==Tipo::Ghost{eficiencia=0.0},
+        Tipo::Poison => if t2==Tipo::Poison || t2==Tipo::Ground || t2==Tipo::Rock || t2==Tipo::Ghost{eficiencia=0.5} else if t2==Tipo::Grass || t2==Tipo::Fairy{eficiencia=2.0} else if t2==Tipo::Steel{eficiencia=0.0},
+        Tipo::Ground => if t2==Tipo::Electric || t2==Tipo::Bug{eficiencia=0.5} else if t2==Tipo::Fire || t2==Tipo::Electric || t2==Tipo::Poison || t2==Tipo::Rock || t2==Tipo::Steel{eficiencia=2.0} else if t2==Tipo::Flying{eficiencia=0.0},
+        Tipo::Flying => if t2==Tipo::Electric || t2==Tipo::Rock || t2==Tipo::Steel{eficiencia=0.5} else if t2==Tipo::Grass || t2==Tipo::Fighting || t2==Tipo::Bug{eficiencia=2.0},
+        Tipo::Psychic => if t2==Tipo::Psychic || t2==Tipo::Steel{eficiencia=0.5} else if t2==Tipo::Fighting || t2==Tipo::Poison{eficiencia=2.0} else if t2==Tipo::Dark{eficiencia=0.0},
+        Tipo::Bug => if t2==Tipo::Fire || t2==Tipo::Fighting || t2==Tipo::Poison || t2==Tipo::Flying || t2==Tipo::Dragon || t2==Tipo::Steel || t2==Tipo::Fairy{eficiencia=0.5} else if t2==Tipo::Grass || t2==Tipo::Psychic || t2==Tipo::Dark{eficiencia=2.0},
+        Tipo::Rock => if t2==Tipo::Fighting || t2==Tipo::Ground || t2==Tipo::Steel{eficiencia=0.5} else if t2==Tipo::Fire || t2==Tipo::Ice || t2==Tipo::Flying || t2==Tipo::Bug{eficiencia=2.0},
+        Tipo::Ghost => if t2==Tipo::Dark{eficiencia=0.5} else if t2==Tipo::Psychic || t2==Tipo::Ghost{eficiencia=2.0} else if t2==Tipo::Normal{eficiencia=0.0},
+        Tipo::Dragon => if t2==Tipo::Steel{eficiencia=0.5} else if t2==Tipo::Ghost{eficiencia=2.0} else if t2==Tipo::Fairy{eficiencia=0.0},
+        Tipo::Dark => if t2==Tipo::Fighting || t2==Tipo::Dark || t2==Tipo::Fairy{eficiencia=0.5} else if t2==Tipo::Psychic || t2==Tipo::Ghost{eficiencia=2.0},
+        Tipo::Steel => if t2==Tipo::Fire || t2==Tipo::Water || t2==Tipo::Electric || t2==Tipo::Steel{eficiencia=0.5} else if t2==Tipo::Ice || t2==Tipo::Rock || t2==Tipo::Fairy {eficiencia=2.0},
+        Tipo::Fairy => if t2==Tipo::Fire || t2==Tipo::Poison || t2==Tipo::Steel{eficiencia=0.5} else if t2==Tipo::Fighting || t2==Tipo::Dragon || t2==Tipo::Dark{eficiencia=2.0},
+    }
+
+    eficiencia
+}
