007b315e291a17a3b70390ab1771d1a6cd4e5667
Oborner <94976381+Oborner@users.noreply.github.com>
2022-09-19 19:55:42 -0300

    versão 2,1

--- src/lib.rs
+++ src/lib.rs
@@ -6,19 +6,6 @@ use std::io;
 use std::str::FromStr;
 use std::fmt::Debug;
 
-pub fn lê_int(msg: &str) -> u32 {
-    println!("{}", msg);
-
-    let mut inteiro = String::new();
-
-    io::stdin()
-        .read_line(&mut inteiro)
-        .expect("Falha na leitura da linha");
-        
-    
-    inteiro.trim().parse().unwrap()
-}
-
 pub fn lê_ok<T: FromStr>() -> T
     where <T as FromStr>::Err: Debug
 {
@@ -33,19 +20,6 @@ pub fn lê_ok<T: FromStr>() -> T
     número.trim().parse().unwrap()
 }
 
-pub fn lê<T: FromStr>(msg: &str) -> Option<T> {
-    let mut número = String::new();
-
-    println!("{}", msg);
-    io::stdin()
-        .read_line(&mut número)
-        .expect("Falha na leitura da linha");
-        
-    match número.trim().parse() {
-        Ok(num) => Some(num),
-        Err(_) => None,
-    }
-} 
 
 }
 
